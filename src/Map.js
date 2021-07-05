488fb1c99d2e250ed2a6ac95439b35fdb82be1ea
Shinjo Melosh <joshin85@gmail.com>
2021-07-05 05:53:45 -0700

    Api call to load cars (#2)
    * Add an API to load cars 
    
    This adds an API call to load cars from an azure function I setup.  The data is stored in azure blob storage - and is updated every 5 days.
    
    This change will:
    * make a call to azure blob storage where the cars are published every 5 days
    * if the call fails, then it will fall back to the current way of loading data
    
    * Update Map.js
    
    undoing google map apikey change

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { GoogleMap, LoadScript, Marker, MarkerClusterer } from '@react-google-maps/api';
 import { cars } from './cars';
+
+// populated by an job thats runs every 5 days
+const carsPath = "https://storageaccountbingb8449.blob.core.windows.net/ctr/car"
+
 const containerStyle = {
   width: `${window.screen.width}px`,
   height: `${window.screen.height-150}px`
@@ -11,10 +15,26 @@ const center = {
   lng: -98
 };
 
- 
+const getCars = () => {
+  return fetch(carsPath, {method: "GET", mode: 'cors'})
+    .then(res => res.json())
+    .then(res => res.data)
+    .catch(err => cars)
+}
+
 export const Map = (props) => {
+  let [ currentCars, setCurrentCars ] = React.useState([]);
+
+  React.useEffect(() => {
+    getCars()
+      .then(fetchedCars => {
+        setCurrentCars(fetchedCars);
+      })
+  }, [])
+
   let dedupe = {}
-  let resCars = cars.filter(car=>{
+
+  let resCars = currentCars.filter(car=>{
       if (props.color && !car.color.toLowerCase().includes(props.color.toLowerCase())){
         return false;
       }
@@ -45,14 +65,14 @@ export const Map = (props) => {
   
   return (
     <LoadScript googleMapsApiKey="AIzaSyA4osQxlfnZEX-CUfopRAxP31PckDPX8vw">
+      { resCars ? 
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
         zoom={4}
        >
-          
         <MarkerClusterer options={options}>
-            {(clusterer)=>{
+            {(clusterer)=> {
                 return resCars.map((marker, i) =>
                 (
                     <Marker 
@@ -66,6 +86,7 @@ export const Map = (props) => {
             }
         </MarkerClusterer>
       </GoogleMap>
+    :<div/>}
     </LoadScript>
   )
 }
